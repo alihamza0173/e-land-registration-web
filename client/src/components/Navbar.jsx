b730af02cec78516e6eb595919af97fe765c2dcf
Ali Hamza <contactalihamza@gmail.com>
2023-11-25 19:14:30 +0500

    login sign up

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -21,7 +21,8 @@ const Navbar = () => {
           <NavBarItem key={item + index} title={item} />
         ))}
         <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
-          Login
+          <a href="./LoginSignUp/loginPage">Login</a>
+          
         </li>
       </ul>
       <div className="flex relative">
