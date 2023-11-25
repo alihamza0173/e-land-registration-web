499ae698ecc3cf36ba1e2a37355760c1b5646606
Shahid Butt <107403832+ShahidButt646@users.noreply.github.com>
2023-11-25 18:55:56 +0500

    updated login routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
 
 import App from "./App";
 import { TransactionsProvider } from "./context/TransactionContext";
 import "./index.css";
 import {initializeDb} from "./config/firebase";
+import ErrorPage from "./error-page";
+import {LoginScreen} from "./components/LoginSignUp/loginPage";
+
 
 // db initialization
 initializeDb();
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginScreen />
+  }
+]);
 
 ReactDOM.render(
   <TransactionsProvider>
-    <App />
+    <RouterProvider router={router} />
   </TransactionsProvider>,
   document.getElementById("root"),
 );
